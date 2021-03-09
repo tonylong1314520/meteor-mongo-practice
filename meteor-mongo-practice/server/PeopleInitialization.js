c39d7607b7050d58161209c503e42bdbfe7dbfe5
Tony Long <tonylong@hawaii.edu>
2021-03-08 21:59:31 -1000

    not done

---
+++ meteor-mongo-practice/server/PeopleInitialization.js
@@ -0,0 +1,6 @@
+import { PeopleCollection } from '../both/PeopleCollection.js';
+
+PeopleCollection.insert({ first: 'Philip', last: 'Johnson', age: 60, city: 'Kailua'});
+PeopleCollection.insert({ first: 'Joanne', last: 'Amberg', age: 58, city: 'Kailua' });
+PeopleCollection.insert({ first: 'Jenna', last: 'Corin', age: 31, city: 'Boulder Creek' });
+PeopleCollection.insert({ first: 'Katie', last: 'Kai', age: 25, city: 'Palo Alto' });
